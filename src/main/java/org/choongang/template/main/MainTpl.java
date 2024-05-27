a18ce1b6c7fa638be8288df56bcac5a7abc9bbc2
hyeon12 <seojh912_@naver.com>
2024-05-27 14:42:31 +0900

    뷰 설계

--- src/main/java/org/choongang/template/main/MainTpl.java
+++ src/main/java/org/choongang/template/main/MainTpl.java
@@ -5,7 +5,7 @@ import org.choongang.template.Template;
 public class MainTpl implements Template {
     //메인쪽 템플릿
     @Override
-    public String getTpl(String tplName) {
+    public String getTpl() {
         StringBuffer sb = new StringBuffer(2000);
         sb.append("메뉴를 선택하세요.\n")
                 .append("1. 회원가입\n")
